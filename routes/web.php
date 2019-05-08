78c2184468005c97fa92200df4f1808eea3b804a
Messias Henrique <messiashenrique1@hotmail.com>
2019-05-08 20:15:38 -0300

    Criação das rotas para as telas do Supervisor #28

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,6 @@ Route::prefix('bolsista')->group(function () {
 });
 
 Route::prefix('usuario')->group(function () {
-	//Route::get('/', 'AdminController@index')->name('admin.dashboard');
-	//Route::get('dashboard', 'AdminController@index')->name('admin.dashboard');
-	//Route::get('register', 'AdminController@create')->name('admin.register');
-	//Route::post('register', 'AdminController@store')->name('admin.register.store');
-	//Route::get('login', 'Auth\Admin\LoginController@login')->name('admin.auth.login');
-	//Route::post('login', 'Auth\Admin\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
-	//Route::post('logout', 'Auth\Admin\LoginController@logout')->name('admin.auth.logout');
 	route::get('/', 'UsuarioController@index')->name('usuario');
 	route::get('login', 'Auth\UsuarioLoginController@login')->name('login-usuario');
 	route::Post('login', 'Auth\UsuarioLoginController@loginUsuario')->name('login-submit');
@@ -37,10 +30,17 @@ Route::prefix('usuario')->group(function () {
 	Route::resource('equipamento', 'EquipamentoController'); 
 	Route::get('lista-equipamento', 'ListarEquipamentoController@index')->name('lista-equipamento-index');
 	route::Post('lista-equipamento', 'EquipamentoController@manutencao')->name('equipamento-manutencao');
-	// route::post('lista-equipamento', 'ListarEquipamentoController@deletaEquipamento')->name('deleta');
 	route::get('lista-problemas', 'ProblemaController@indexLista')->name('lista-problemas');
 	route::Post('lista-problemas', 'ProblemaController@chamado')->name('problema-chamado');
 
+});
+
+Route::prefix('supervisor')->group(function () {
+	route::get('/', 'SupervisorController@index')->name('bolsista');
+	route::get('login', 'Auth\SupervisorLoginController@login')->name('login-supervisor');
+	route::Post('login', 'Auth\SupervisorLoginController@loginBolsista')->name('login-submit-supervisor');
+	route::get('register', 'SupervisorController@registerIndex')->name('supervisor-b');
+	route::Post('register', 'SupervisorController@registerBolsista')->name('register-supervisor');
 });
 
 Route::post('logout', 'Auth\LoginController@logout');
