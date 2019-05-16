eb4bbf68dc9849654172ed6c20f09b15b038d05b
João Victor <victor_campelo@outlook.com>
2019-05-16 18:18:36 -0300

    small modifications in the col-md

--- resources/views/supervisor/register-bolsista.blade.php
+++ resources/views/supervisor/register-bolsista.blade.php
@@ -18,7 +18,7 @@
     <div class="col-md-8">
       <div class="card">
 
-        <div class="card-body">
+        <div class="container">
           <form method="POST" action="{{ route('supervisor-register-bolsista') }}">
             @csrf
             @if ($errors->has('errors'))
@@ -27,7 +27,7 @@
             </span>
             @endif
             <div class="form-group row">
-              <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+              <label for="name" class="col-md-2 col-form-label text-md-right">{{ __('Name') }}</label>
 
               <div class="col-md-6">
                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -35,7 +35,7 @@
             </div>
 
             <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+              <label for="email" class="col-md-2 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
               <div class="col-md-6">
                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -45,7 +45,7 @@
             </div>
 
             <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+              <label for="password" class="col-md-2 col-form-label text-md-right">{{ __('Password') }}</label>
 
               <div class="col-md-6">
                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
@@ -56,7 +56,7 @@
 
 
             <div class="form-group row">
-              <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Cpf') }}</label>
+              <label for="name" class="col-md-2 col-form-label text-md-right">{{ __('Cpf') }}</label>
 
               <div class="col-md-6">
                 <input id="cpf" type="text" class="form-control{{ $errors->has('cpf') ? ' is-invalid' : '' }}" name="cpf" value="{{ old('cpf') }}" required autocomplete="cpf" autofocus>
@@ -65,8 +65,9 @@
               </div>
             </div>
 
+            <div class="card">
             <div class="form-group row mb-0">
-              <div class="col-md-6 offset-md-4">
+              <div class="col-md-6 offset-md-2">
                 <button type="submit" class="btn btn-primary">
                   {{ __('Register') }}
                 </button>
