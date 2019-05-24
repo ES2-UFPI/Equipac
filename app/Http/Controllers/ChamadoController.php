b7f0f0dc5ab0fea48ee394eae1572b7780c4e484
Messias Henrique <messiashenrique1@hotmail.com>
2019-05-24 10:39:20 -0300

    Eloquent ORP to "Chamados" and your derivates add.

--- app/Http/Controllers/ChamadoController.php
+++ app/Http/Controllers/ChamadoController.php
@@ -4,6 +4,8 @@ namespace equipac\Http\Controllers;
 
 use Illuminate\Http\Request;
 use equipac\models\problema;
+use equipac\models\Chamados;
+use equipac\models\Status_chamado;
 
 class ChamadoController extends Controller
 {
@@ -19,10 +21,10 @@ class ChamadoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Problema $prob)
+    public function index(Chamados $prob)
     {
-        $problema = $prob::all();
-        return view('bolsista.chamados', compact('problema'));
+        $chamado = $prob::all();
+        return view('bolsista.chamados', compact('chamado'));
     }
 
     /**
@@ -104,5 +106,20 @@ class ChamadoController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function alterarStatus(Request $request, Status_chamado $status, Chamados $cham)
+    {
+        $chamado = $cham::find($request->id);
+        $sts = $status::find($request->status);
+        $chamado->status()->associate($sts);
+        if ($chamado->save()) {
+            return redirect()
+            ->route('index-chamado')
+            ->with('success', 'Chamado alterado com sucesso!');
+        }
+        return redirect()
+        ->back()
+        ->with('error', 'Falha ao Cadastrar');
     }
 }
--- resources/views/bolsista/chamados.blade.php
+++ resources/views/bolsista/chamados.blade.php
@@ -25,20 +25,41 @@
         <table class="table table-hover">
           <tr>
             <th>Id Chamado</th>
-            <th>Descrição</th>
-            <th>Data</th>
-            <th></th>
+            <th>Id Usuario</th>
+            <th>nome</th>
+            <td>Status</td>
+            <td>Opcão</td>
           </tr>
-          @foreach($problema as $e )
+          @foreach($chamado as $index => $ma)
           <tr>
-            <th>{{ $e['ID']}}</th>
-            <th>{{ $e['Descricao']}}</th>
-            <th>{{ $e['Criacao']}}</th>
-            <th><button type="" class="btn btn-primary">Sol. Chamado</button></th>
-          </tr>
-          @endforeach
-        </table>
-      </div>
+          @if($ma['status_id']!='4')
+          <th>{{ $ma->id}}</th>
+          <th>{{ $ma->problema->usuario->id  }}</th>
+          <th>{{ $ma->problema->usuario->nome  }}</th>
+          <th>{{ $ma->status->name  }}</th>
+          <td>
+            <form method="post" action="{{route('altera-status-chamado')}}">
+              {!! csrf_field() !!}
+              @if($ma['status_chamado_id']=='1')
+              <input type="hidden" name="id" value="{{$ma['id']}}">
+              <input type="hidden" name="status" value="2">
+              <button type="imput" class="btn btn-primary">Atribuir</button></th>
+              @elseif($ma['status_chamado_id']=='2')
+              <input type="hidden" name="id" value="{{$ma['id']}}">
+              <input type="hidden" name="status" value="3">
+              <button type="imput" class="btn btn-primary">Em andamento</button></th>
+              @elseif($ma['status_chamado_id']=='3')
+              <input type="hidden" name="id" value="{{$ma['id']}}">
+              <input type="hidden" name="status" value="4">
+              <button type="imput" class="btn btn-primary">Concluir</button></th>
+              @endif
+            </form>
+          </td>
+          @endif
+        </tr>
+        @endforeach
+      </table>
+    </div>
       <!-- /.card-body -->
     </div>
     <!-- /.card -->
