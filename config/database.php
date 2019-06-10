db764b985842e4490ab740d716f1f5f08d37c78f
João Victor <victor_campelo@outlook.com>
2019-06-10 15:22:01 -0300

    mysql test travis 2

--- config/database.php
+++ config/database.php
@@ -61,6 +61,18 @@ return [
             ]) : [],
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', '127.0.0.1'),
+            'database' => env('DB_TEST_DATABASE', 'equipac_ci'),
+            'username' => env('DB_TEST_USERNAME', 'root'),
+            'password' => env('DB_TEST_PASSWORD', 'root'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
